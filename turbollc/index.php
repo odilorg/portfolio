cd57fdf25885d0e494a7e6466afb6a311e2fa260
Odil Jaha <odilorg@gmail.com>
2020-11-03 16:43:55 +0500

    space del

--- turbollc/index.php
+++ turbollc/index.php
@@ -2,47 +2,23 @@
 
 ?>
 <!doctype html>
-
 <html lang="en">
-
   <head>
-
     <!-- Required meta tags -->
-
     <meta charset="utf-8">
-
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-
-
     <!-- Bootstrap CSS -->
-
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-
-
-
     <title>Turbollc - Dispatch</title>
-
   </head>
-
   <?php
-
 	if(isset($_SESSION['valid'])) {			
-
 		include("connection.php");					
-
 		$result = mysqli_query($mysqli, "SELECT * FROM login");
-
 	?>
-
-
-
   <body>
-
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
   <a class="navbar-brand" href="#">
-
   <img src="images/logo.jpg" width="30" height="30" class="d-inline-block align-top" alt="" loading="lazy">
 
   Welcome <?php echo $_SESSION['name'] ?> !
